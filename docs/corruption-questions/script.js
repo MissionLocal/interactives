3af5a161217969fd72e6308f863e7786a7a17089
WillJarrett1 <73312768+WillJarrett1@users.noreply.github.com>
2021-11-03 00:31:03 -0700

    scriptSeparate

---
+++ docs/corruption-questions/script.js
@@ -0,0 +1,19 @@
+document.getElementById("q1").addEventListener("click", function() {
+    document.getElementById("q1-text").innerHTML = '<p style="text-align: left">All of them. At least 8 people, plus the company Recology, have pleaded guilty to charges related to the city hall corruption probe. They are all directly related to Mohammed Nuru.</p>';
+  });
+  
+  document.getElementById("q2").addEventListener("click", function() {
+    document.getElementById("q2-text").innerHTML = '<p style="text-align: left">Construction company owner Florence Kong pleaded guilty to bribing Nuru with a $40,000 Rolex (alongside meals, cash, and a gate for his vacation home).</p>';
+  });
+  
+  document.getElementById("q3").addEventListener("click", function() {
+    document.getElementById("q3-text").innerHTML = "<p style='text-align: left'>Bernie Curran signed off on multiple permits for major work on Harlan Kelly's house.</p>";
+  });
+  
+  document.getElementById("q4").addEventListener("click", function() {
+    document.getElementById("q4-text").innerHTML = "<p style='text-align: left'>In 2015, then-supervisor London Breed received a $1,250 campaign donation from Nick Bovis in order to secure a float in the Pride parade. The donation was not reported by Breed's campaign and exceeded the $500 maximum.</p>";
+  });
+  
+  document.getElementById("q5").addEventListener("click", function() {
+    document.getElementById("q5-text").innerHTML = '<p style="text-align: left">We think the answer is three – moving from Hernandez to Mohammed Nuru, to Tom Hui, to Santos.</p>';
+  });
