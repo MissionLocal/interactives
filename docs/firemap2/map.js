43aa7b075f8540c2502b3f82c13d6af86432cb8b
WillJarrett1 <73312768+WillJarrett1@users.noreply.github.com>
2021-11-08 17:18:46 -0800

    newFiremap

--- docs/firemap2/map.js
+++ docs/firemap2/map.js
@@ -1,24 +1,18 @@
 mapboxgl.accessToken = "pk.eyJ1IjoibWxub3ciLCJhIjoiY2t0d2FsdWRpMmkxbDMxcnJ4eTNsMmFlMiJ9.dUju5BD_HqseLNWGIGvXpg";
 
-// define outer bounds of map
-const bounds = [
-  [-122.8, 37.5], // southwest coordinate
-  [-122, 38] // northeast coordinate
-  ];
-
 // define basemap
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mlnow/ckunawnac3rtn17s7xksr52md',
-  zoom: 13.5,
-  center: [-122.415, 37.755],
-  maxBounds: bounds
+  zoom: 11.5,
+  center: [-122.43, 37.76],
 });
 
 // function to define color scale for map layers
 function fillColorFunction(fillColorBin) {
   fillColor =  ["match",["get", fillColorBin],
-    "trash","#F39C12",
+    "trash","#FFC550",
+    "structure","#EC7063",
     "encampment","#A569BD",
     "vehicle","#58D68D",
     "other","#7F8C8D",
@@ -27,25 +21,25 @@ function fillColorFunction(fillColorBin) {
 }
 
 // function to define map layers information
-function mapDetailsFunction(mapID, visibility) {
+function mapDetailsFunction(mapID, visibility, source) {
   mapDetails = {
     id: mapID,
     type: "circle",
     source: {
       type: "geojson",
-      data: "fires2021MissionExtinguish.geojson",
+      data: source,
     },
     layout: {
       'visibility': visibility
       },
     paint: {
       "circle-color": fillColor,
-      "circle-opacity": 1,
+      "circle-opacity": 0.8,
       'circle-radius': {
-        'base': 2,
+        'base': 1.75,
         'stops': [
-        [16, 5],
-        [22, 180]
+        [12, 6],
+        [22, 220]
         ]
         },
     },
@@ -56,59 +50,187 @@ function mapDetailsFunction(mapID, visibility) {
 // load my map layers
 map.on("load", function () {
   fillColorFunction("type");
-  mapDetailsFunction("fires_id", "visible");
+  mapDetailsFunction("other", "visible", "fires2021_other.geojson");
+    map.addLayer(mapDetails);
+  mapDetailsFunction("vehicle", "visible", "fires2021_vehicle.geojson");
+    map.addLayer(mapDetails);
+  mapDetailsFunction("structure", "visible", "fires2021_structure.geojson");
+    map.addLayer(mapDetails);
+  mapDetailsFunction("encampment", "visible", "fires2021_encampment.geojson");
+    map.addLayer(mapDetails);
+  mapDetailsFunction("trash", "visible", "fires2021_trash.geojson");
     map.addLayer(mapDetails);
 });
 
-// define legend
-// define layer names
-const layers = [
-  'Trash',
-  'Encampment',
-  'Vehicle',
-  'Other'
-  ];
-  const colors = [
-  '#F39C12',
-  '#A569BD',
-  '#58D68D',
-  '#7F8C8D'
-  ];
-   
-  // create legend
-  const legend = document.getElementById('legend');
+		// radio button control
+		document.getElementById('legend').addEventListener('change', (event) => {
+			const type = event.target.value;
+			// update the map filter
+			if (type === 'all') {
+				map.setLayoutProperty('trash','visibility','visible');
+				map.setLayoutProperty('structure','visibility','visible');
+        map.setLayoutProperty('encampment','visibility','visible');
+				map.setLayoutProperty('vehicle','visibility','visible');
+				map.setLayoutProperty('other','visibility','visible');
+			} else if (type === 'trash') {
+				map.setLayoutProperty('trash','visibility','visible');
+				map.setLayoutProperty('structure','visibility','none');
+        map.setLayoutProperty('encampment','visibility','none');
+				map.setLayoutProperty('vehicle','visibility','none');
+				map.setLayoutProperty('other','visibility','none');
+      } else if (type === 'structure') {
+				map.setLayoutProperty('trash','visibility','none');
+				map.setLayoutProperty('structure','visibility','visible');
+        map.setLayoutProperty('encampment','visibility','none');
+				map.setLayoutProperty('vehicle','visibility','none');
+				map.setLayoutProperty('other','visibility','none');
+      } else if (type === 'encampment') {
+				map.setLayoutProperty('trash','visibility','none');
+				map.setLayoutProperty('structure','visibility','none');
+        map.setLayoutProperty('encampment','visibility','visible');
+				map.setLayoutProperty('vehicle','visibility','none');
+				map.setLayoutProperty('other','visibility','none');
+			} else if (type === 'vehicle') {
+				map.setLayoutProperty('trash','visibility','none');
+				map.setLayoutProperty('structure','visibility','none');
+        map.setLayoutProperty('encampment','visibility','none');
+				map.setLayoutProperty('vehicle','visibility','visible');
+				map.setLayoutProperty('other','visibility','none');
+			} else if (type === 'other') {
+				map.setLayoutProperty('trash','visibility','none');
+				map.setLayoutProperty('structure','visibility','none');
+        map.setLayoutProperty('encampment','visibility','none');
+				map.setLayoutProperty('vehicle','visibility','none');
+				map.setLayoutProperty('other','visibility','visible');
+			}
+		});
 
-  layers.forEach((layer, i) => {
-  const color = colors[i];
-  const item = document.createElement('div');
-  const key = document.createElement('span');
-  key.className = 'legend-key';
-  key.style.backgroundColor = color;
-   
-  const value = document.createElement('span');
-  value.innerHTML = `${layer}`;
-  item.appendChild(key);
-  item.appendChild(value);
-  legend.appendChild(item);
-  });
-
-// Create the popup - population
-map.on('click', 'fires_id', function (e) {
+// Create the popup - other
+map.on('click', 'other', function (e) {
     var date = e.features[0].properties.date_string;
     var situation = e.features[0].properties.situation;
-    var id = e.features[0].properties.id;
+    var address = e.features[0].properties.address;
+    var property_use = e.features[0].properties.property_use;
     var response = e.features[0].properties.time_to_respond_string;
+    /*var id = e.features[0].properties.id;*/
     new mapboxgl.Popup()
         .setLngLat(e.lngLat)
         .setHTML('<h4>'+date+'</h4>'
             + '<p><strong>Situation</strong>: '+situation+'</p>'
+            + '<p><strong>Address</strong>: '+address+'</p>'
+            + '<p><strong>Property use</strong>: ' +property_use+'</p>'
             + '<p><strong>Response time</strong>: ' +response+'</p>'
-            + '<p><strong>Incident ID</strong>: '+id+'</p>')
+            /*+ '<p><strong>Incident ID</strong>: '+id+'</p>'*/
+            )
         .addTo(map);
 });
-map.on('mouseenter', 'fires_id', function () {
+map.on('mouseenter', 'other', function () {
     map.getCanvas().style.cursor = 'pointer';
 });
-map.on('mouseleave', 'fires_id', function () {
+map.on('mouseleave', 'other', function () {
     map.getCanvas().style.cursor = '';
+});
+
+// Create the popup - vehicle
+map.on('click', 'vehicle', function (e) {
+  var date = e.features[0].properties.date_string;
+  var situation = e.features[0].properties.situation;
+  var address = e.features[0].properties.address;
+  var property_use = e.features[0].properties.property_use;
+  var response = e.features[0].properties.time_to_respond_string;
+  /*var id = e.features[0].properties.id;*/
+  new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML('<h4>'+date+'</h4>'
+          + '<p><strong>Situation</strong>: '+situation+'</p>'
+          + '<p><strong>Address</strong>: '+address+'</p>'
+          + '<p><strong>Property use</strong>: ' +property_use+'</p>'
+          + '<p><strong>Response time</strong>: ' +response+'</p>'
+          /*+ '<p><strong>Incident ID</strong>: '+id+'</p>'*/
+          )
+      .addTo(map);
+});
+map.on('mouseenter', 'vehicle', function () {
+  map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'vehicle', function () {
+  map.getCanvas().style.cursor = '';
+});
+
+// Create the popup - structure
+map.on('click', 'structure', function (e) {
+  var date = e.features[0].properties.date_string;
+  var situation = e.features[0].properties.situation;
+  var address = e.features[0].properties.address;
+  var property_use = e.features[0].properties.property_use;
+  var response = e.features[0].properties.time_to_respond_string;
+  /*var id = e.features[0].properties.id;*/
+  new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML('<h4>'+date+'</h4>'
+          + '<p><strong>Situation</strong>: '+situation+'</p>'
+          + '<p><strong>Address</strong>: '+address+'</p>'
+          + '<p><strong>Property use</strong>: ' +property_use+'</p>'
+          + '<p><strong>Response time</strong>: ' +response+'</p>'
+          /*+ '<p><strong>Incident ID</strong>: '+id+'</p>'*/
+          )
+      .addTo(map);
+});
+map.on('mouseenter', 'structure', function () {
+  map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'structure', function () {
+  map.getCanvas().style.cursor = '';
+});
+
+// Create the popup - encampment
+map.on('click', 'encampment', function (e) {
+  var date = e.features[0].properties.date_string;
+  var situation = e.features[0].properties.situation;
+  var address = e.features[0].properties.address;
+  var property_use = e.features[0].properties.property_use;
+  var response = e.features[0].properties.time_to_respond_string;
+  /*var id = e.features[0].properties.id;*/
+  new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML('<h4>'+date+'</h4>'
+          + '<p><strong>Situation</strong>: '+situation+'</p>'
+          + '<p><strong>Address</strong>: '+address+'</p>'
+          + '<p><strong>Property use</strong>: ' +property_use+'</p>'
+          + '<p><strong>Response time</strong>: ' +response+'</p>'
+          /*+ '<p><strong>Incident ID</strong>: '+id+'</p>'*/
+          )
+      .addTo(map);
+});
+map.on('mouseenter', 'encampment', function () {
+  map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'encampment', function () {
+  map.getCanvas().style.cursor = '';
+});
+
+// Create the popup - trash
+map.on('click', 'trash', function (e) {
+  var date = e.features[0].properties.date_string;
+  var situation = e.features[0].properties.situation;
+  var address = e.features[0].properties.address;
+  var property_use = e.features[0].properties.property_use;
+  var response = e.features[0].properties.time_to_respond_string;
+  /*var id = e.features[0].properties.id;*/
+  new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML('<h4>'+date+'</h4>'
+          + '<p><strong>Situation</strong>: '+situation+'</p>'
+          + '<p><strong>Address</strong>: '+address+'</p>'
+          + '<p><strong>Property use</strong>: ' +property_use+'</p>'
+          + '<p><strong>Response time</strong>: ' +response+'</p>'
+          /*+ '<p><strong>Incident ID</strong>: '+id+'</p>'*/
+          )
+      .addTo(map);
+});
+map.on('mouseenter', 'trash', function () {
+  map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'trash', function () {
+  map.getCanvas().style.cursor = '';
 });
