351a67abc124811deb5e4ce652a33aa5934bdbc1
WillJarrett1 <73312768+WillJarrett1@users.noreply.github.com>
2021-11-30 14:29:42 -0800

    BoudinUpdate

--- docs/boudin-recall/d3.js
+++ docs/boudin-recall/d3.js
@@ -1,9 +1,9 @@
 (function () {
 
   // Margin convention
-  const margin = { top: 150, right: 100, bottom: 100, left: 230 }
-  const width = 660 - margin.left - margin.right
-  const height = 1200 - margin.top - margin.bottom
+  const margin = { top: 150, right: 70, bottom: 150, left: 230 }
+  const width = 725 - margin.left - margin.right
+  const height = 1300 - margin.top - margin.bottom
 
   // Search
   d3.select("#input").on('keyup', function() {
@@ -12,26 +12,26 @@
     d3.selectAll("circle").filter(d => d.name.toLocaleUpperCase().indexOf(searchTerm) == -1).attr('fill', '#bdbdbd')
   })
 
-  // Create time parser
-  const tParser = d3.timeParse("%Y-%m-%d")
-
   // Define positions
   const positions = ["Oppose","Support"]
 
   // Define x axis position
   const xPositionScale = d3.scalePoint()
     .domain(positions)
-    .range([-50, width - 95])
+    .range([-55, width - 130])
 
   // Define y axis position
   const yPositionScale = d3.scaleTime()
     .domain([new Date("2021-02-08"), new Date("2021-10-31")])
-    .range([0, height])
+    .range([0, height - 75])
+
+  var occupation_labels = ['Investment','Law',"'Entity'",'Unemployed','Retired','Executive','Other']
+  var occupation_colors = ["#ff6e54","#ffa600","#85C1E9",'#444e86','#003f5c','#955196',"#747474"]
 
   // Define colour scale
   const colorScale = d3.scaleOrdinal()
-    .domain(['investment','law',"entity",'unemployed','retired','executive','other'])
-    .range(["#ff6e54","#ffa600","#85C1E9",'#444e86','#003f5c','#955196',"#747474"])
+    .domain(occupation_labels)
+    .range(occupation_colors)
 
   // Set radius scale
   const radiusScale = d3.scaleSqrt()
@@ -62,6 +62,7 @@
       d.y = d.y + d.vy;
     })
 
+    // Create svg
     const svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -74,7 +75,7 @@
       .join('text')
       .attr('x', d => xPositionScale(d))
       .attr('dx', 25)
-      .attr('dy', -85)
+      .attr('dy', -100)
       .attr("class", "axis_labels_header")
       .text(d => d)
 
@@ -93,12 +94,36 @@
       .attr("stroke", '#00000000')
       .attr("paint-order", "stroke");
 
-    // create y-axis
+    // Create y-axis
     var y_axis = d3.axisLeft().scale(yPositionScale);
     svg.append("g")
       .attr("transform", "translate(-140, 0)")
       .attr("class", "axis_labels")
       .call(y_axis);
+    
+    // Create legend
+    var n = occupation_labels.length / 2
+    var itemWidth = 120
+    var itemHeight = 40
+
+    var legend = svg.selectAll(".legend")
+      .data(occupation_labels)
+      .enter()
+      .append("g")
+      .attr("transform", function(d,i) { return "translate(" + i%n * itemWidth + "," + Math.floor(i/n) * itemHeight + ")"; })
+      .attr("class","legend");
+
+    legend.append('rect')
+      .attr("width", 15)
+      .attr("height", 15)
+      .attr("x", "-60")
+      .attr("y", "1035")
+      .attr("fill", function(d,i) { return colorScale(i); });
+
+    legend.append('text')
+      .attr("x", "-35")
+      .attr("y", "1048")
+      .text(function(d) { return d; });
     
     // Trigger tooltip
     d3.selectAll("circle")
