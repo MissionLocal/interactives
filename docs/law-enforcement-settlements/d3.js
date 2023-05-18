f16c7e160cf13073523e12ff3c432690a64dd4f8
WillJarrettData <73312768+WillJarrettData@users.noreply.github.com>
2023-05-17 18:21:19 -0700

    pym

--- docs/law-enforcement-settlements/d3.js
+++ docs/law-enforcement-settlements/d3.js
@@ -1,3 +1,6 @@
+// Set up pym
+var pymChild = new pym.Child();
+
 // Margin convention
 const margin = { top: 50, right: 50, bottom: 50, left: 50 };
 const width = 800 - margin.left - margin.right;
@@ -370,3 +373,5 @@ function moveCircles(position) {
     rejigLabels("hidden", "hidden", "visible")
   }
 }
+
+pymChild.sendHeight();
