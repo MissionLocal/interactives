b0d3f0faf47318461adeb8b59558233c4759ede2
agnesxlu <90271488+agnesxlu@users.noreply.github.com>
2023-10-24 11:34:18 -0700

    add halloween map files

---
+++ docs/map.js
@@ -0,0 +1,61 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoibWxub3ciLCJhIjoiY2t0dnZwcm1mMmR5YzMycDNrcDZtemRybyJ9.Br-G0LTOB3M6w83Az4XGtQ';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mlnow/clo3ra7ut003y01r2eok49ae7',
+    zoom: 11.5,
+    center: [-122.436, 37.76]
+});
+
+map.on('load', function () {
+    map.addSource('lines', {
+        type: 'geojson',
+        data: 'data.geojson'
+    });
+
+    // Add a layer to display the lines
+    map.addLayer({
+        id: 'streets',
+        type: 'line',
+        source: 'lines',
+        layout: {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        paint: {
+            'line-color': '#efbe25', // Line color
+            'line-width': 2.5 // Line width
+        }
+    });
+});
+
+
+
+// Add a popup to display information on hover for point features
+map.on('click', 'streets', function (e) {
+    const feature = e.features[0];
+
+    // Extract the properties you want to display in the popup
+    const event = feature.properties["Event"];
+    const dtime = feature.properties["When"];
+    const location = feature.properties["Where"];
+
+    // Get the coordinates of the point feature
+    const coordinates = feature.geometry.coordinates.slice();
+
+    new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(
+            '<p><strong>Event</strong>: ' + event +
+            '<p><strong>When</strong>: ' + dtime +
+            '<p><strong>Where</strong>: ' + location
+        )
+        .addTo(map);
+});
+
+map.on('mouseenter', 'event_locations', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'event_locations', function () {
+    map.getCanvas().style.cursor = '';
+});
+
