9074d891f7a33cc4361d6f13f706126f9b79505e
WillJarrettData <73312768+WillJarrettData@users.noreply.github.com>
2023-10-30 14:49:55 -0700

    Update script.js

--- docs/halloween-crossword-2023/script.js
+++ docs/halloween-crossword-2023/script.js
@@ -779,7 +779,7 @@ var answers = [
         "direction": "down",
         "word": "DOPY",
         "cells": ["4-0", "5-0", "6-0", "7-0"],
-        'clue': "Howling at the sea"
+        'clue': "A little dim"
     },
     {
         "index": 55,
