091300aa3f159d25b32df949cdf48185f676ad5a
WillJarrettData <73312768+WillJarrettData@users.noreply.github.com>
2024-02-15 11:14:09 -0800

    meetTheCandidatesMap

---
+++ docs/meet-the-candidates-map/map.js
@@ -0,0 +1,129 @@
+mapboxgl.accessToken = "pk.eyJ1IjoibWxub3ciLCJhIjoiY2t0d2FsdWRpMmkxbDMxcnJ4eTNsMmFlMiJ9.dUju5BD_HqseLNWGIGvXpg";
+
+        // define basemap
+        var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mlnow/clsnkxahw00nj01r9apk2awp0',
+        zoom: 10.9,
+        pitch: 0, // pitch in degrees
+        bearing: 0, // bearing in degrees
+        center: [-122.43, 37.77]
+        });
+
+        // function to define map layers information
+        function mapLineFunction(mapID, visibility, source) {
+            map.addLayer({
+                id: mapID,
+                type: "line",
+                source: {
+                    type: "geojson",
+                    data: source,
+                },
+                layout: {
+                    'visibility': visibility
+                },
+                paint: {
+                    "line-color": "black",
+                    "line-opacity": 1,
+                    "line-width": 3.3
+                }
+            });
+        }
+
+        // function to define map fill information
+        function mapFillFunction(mapID, visibility, source) {
+            map.addLayer({
+                id: mapID,
+                type: "fill",
+                source: {
+                    type: "geojson",
+                    data: source,
+                },
+                layout: {
+                    'visibility': visibility
+                },
+                paint: {
+                    // determine color by properties.DISTRICT value
+                    "fill-color": ["match", ["get", "DISTRICT"],
+                        "1", "#6b92ff",
+                        "3", "#cb5074",
+                        "5", "#00813d",
+                        "7", "#ee6d65",
+                        "9", "#ffc352",
+                        "11", "#ff6fae",
+                        "#000000"
+                    ],
+                    "fill-opacity": [
+                        'case',
+                        ['boolean', ['feature-state', 'hover'], false],
+                        1,
+                        0.7
+                    ],
+                }
+            });
+        }
+
+        // function to define district label information
+        function mapLabelFunction() {
+            map.addLayer({
+                id: "labels",
+                type: "symbol",
+                source: {
+                    'type': "geojson",
+                    'data': "map4ALabels.geojson",
+                },
+                layout: {
+                    'visibility': "visible",
+                    'text-field': ['get', 'name'],
+                    'text-size': 28,
+                },
+                paint: {
+                "text-color": 'black',
+            }
+            })
+        }
+
+        // load my map layers
+        map.on("load", function () {
+            map.addSource('map_fill_source', {
+                type: 'geojson',
+                data: 'districtMap.geojson'
+            });
+
+            mapLineFunction("map_line", "none", 'districtMap.geojson');
+            mapFillFunction("map_fill", "visible", 'districtMap.geojson');
+            mapLabelFunction();
+        });
+
+        // number formatting function
+        function separator(numb) {
+            var str = numb.toString().split(".");
+            str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            return str.join(".");
+        }
+
+        // send user to url
+        map.on('click', 'map_fill', function (e) {
+            var feature = e.features[0];
+            var district = feature.properties.DISTRICT;
+            if (district == "9") {
+                window.location.href = "https://missionlocal.org/2024/01/meet-the-candidates-2024-district-9-supervisor-election-questions-answers-archive/";
+            } else {
+                window.location.href = "https://www.google.com/";
+            }
+            
+        });
+        map.on('mouseenter', 'map_fill', function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+        map.on('mouseleave', 'map_fill', function () {
+            map.getCanvas().style.cursor = '';
+        });
+
+        // add navigation controls
+        map.addControl(new mapboxgl.NavigationControl());
+
+        // resize map when window is resized
+        this.map.once('load', () => {
+            this.map.resize();
+        });
