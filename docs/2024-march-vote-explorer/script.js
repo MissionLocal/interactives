a3103d5d7a0f93d4b7067195308006b4493b75ad
WillJarrettData <73312768+WillJarrettData@users.noreply.github.com>
2024-03-07 14:07:17 -0800

    autofillUpdate

--- docs/2024-march-vote-explorer/script.js
+++ docs/2024-march-vote-explorer/script.js
@@ -16,16 +16,31 @@ var map = new mapboxgl.Map({
     center: [-122.438, mapY],
 });
 
-// define fill and source for geography
+// define stuff
 var mapFill = 'map_fill_001'
 var source = 'basemap'
-
-// define other stuff
 var selectedAreas = []
-var legendDetails = document.getElementById("legend-details")
+var legendDetailsLocal = document.getElementById("legend-details-local")
+var legendDetailsTotal = document.getElementById("legend-details-total")
 var results = document.getElementById('results')
 var areaList = document.getElementById('area-list')
+var dropdown = document.getElementById('dataset-dropdown');
 var pymChild = new pym.Child();
+const fullnames = {
+    'propA': ['Proposition A', 'Affordable housing bond'],
+    'propB': ['Proposition B', 'Police officer staffing'],
+    'propC': ['Proposition C', 'Transfer tax exemption'],
+    'propD': ['Proposition D', 'Tightened ethics rules'],
+    'propE': ['Proposition E', 'Loosened police rules'],
+    'propF': ['Proposition F', 'Welfare drug screening'],
+    'propG': ['Proposition G', 'Algebra in 8th grade'],
+    'dccc17': ['DCCC Assembly District 17', ''],
+    'dccc19': ['DCCC Assembly District 19', ''],
+    'assembly17': ['Assembly District 17', ''],
+    'assembly19': ['Assembly District 19', ''],
+    'court1': ['Superior Court Seat 1', ''],
+    'court13': ['Superior Court Seat 13', ''],
+    'turnout': ['Turnout', '']}
 
 ///
 /// PRIMARY FUNCTIONS
@@ -33,167 +48,179 @@ var pymChild = new pym.Child();
 
 // main function
 async function main() {
-    const fullnames = {
-        'propA': ['Proposition A', 'Affordable housing bond'],
-        'propB': ['Proposition B', 'Police officer staffing'],
-        'propC': ['Proposition C', 'Transfer tax exemption'],
-        'propD': ['Proposition D', 'Tightened ethics rules'],
-        'propE': ['Proposition E', 'Loosened police rules'],
-        'propF': ['Proposition F', 'Welfare drug screening'],
-        'propG': ['Proposition G', 'Algebra in 8th grade'],
-        'dccc17': ['DCCC Assembly District 17', ''],
-        'dccc19': ['DCCC Assembly District 19', ''],
-        'assembly17': ['Assembly District 17', ''],
-        'assembly19': ['Assembly District 19', ''],
-        'court1': ['Superior Court Seat 1', ''],
-        'court13': ['Superior Court Seat 13', ''],
-        'turnout': ['Turnout', '']}
-
     const datasets = await fetchData(Object.keys(fullnames));
-    
-    var total_voters = Object.values(datasets['turnout']['votes_cast']).reduce((acc, value) => acc + value, 0);
-    legendDetails.innerHTML = "<span class='local-highlight legend-label'>0</span> out of <span class='overall-highlight legend-label'>" + numberWithCommas(total_voters) + "</span>"
+    const lookup = await fetchCSV('lookup');
+    const total_voters = Object.values(datasets['turnout']['votes_cast']).reduce((acc, value) => acc + value, 0);
 
-    // when something is clicked, trigger interaction code
+    // set total voters
+    legendDetailsTotal.innerHTML = numberWithCommas(total_voters);
+
+    // when something on the map is clicked, trigger interaction code
     map.on('click', mapFill, (e) => {
-        onInteraction(datasets, fullnames);
+        hoveredId = e.features[0].properties.precinct;
+        onMapClick(datasets, fullnames, hoveredId);
+    });
+
+    // when something on the dropdown is clicked, trigger interaction code
+    dropdown.addEventListener('change', function() {
+        onDropdownSelect(datasets, fullnames, lookup, this.value);
     });
 }
 
-// do everything when something is clicked
-function onInteraction(datasets, fullnames) {
+// when map is clicked
+function onMapClick(datasets, fullnames, hoveredId) {
 
-    // when area changes, select or deselect area as needed
     if (selectedAreas.includes(hoveredId)) {
         removeItem(selectedAreas, hoveredId)
+        changeMapSelection([hoveredId], false);
     } else {
         selectedAreas.push(hoveredId)
+        changeMapSelection([hoveredId], true);
     }
-    changeAreaSelect(source, hoveredId, selectedAreas.includes(hoveredId))
 
-    // clear the results
-    results.innerHTML = ""
+    generate(datasets, fullnames, selectedAreas);
+}
 
-    // check list is not empty - if it is not, run the rest of the code
-    if (validation(datasets['turnout'])) {
+// when dropdown is clicked
+function onDropdownSelect(datasets, fullnames, lookup, value) {
 
-        // set up the list of areas at the bottom of the map
-        let areaListHTML = ""
-        for (let i = 0; i < selectedAreas.length; i++) {
-            areaListHTML += "<span class='area'>" + selectedAreas[i] + "</span>"
+    selectedAreas = lookup.filter(function (el) {
+        return el.district == value;
+    }).map(function (el) {
+        return el.precinct;
+    });
+    var allAreas = lookup.map(function (el) {
+        return el.precinct;
+    }).slice(1);
+
+    changeMapSelection(allAreas, false);
+    if (value == 'custom') {
+        clear();
+    } else {
+        changeMapSelection(selectedAreas, true);
+    }
+
+    generate(datasets, fullnames, selectedAreas);
+}
+
+// function to change map selection
+function changeMapSelection(areas, bool) {
+    areas.forEach(function (area) {
+        if (area == undefined) {
+            return
         }
-        areaList.innerHTML = areaListHTML + "<button id='clear-button'>Clear selection</button>"
-            
-        // create clear button
-        var clearButton = document.getElementById("clear-button")
-        clearButton.addEventListener("click", clear);
+        map.setFeatureState(
+            { source: source, id: area },
+            { selected: bool }
+        );
+    });
+}
 
-        ///
-        /// calculate rate values
-        ///
+function generate(datasets, fullnames, selectedAreas) {
+    results.innerHTML = "" // clear the results
 
-        // iterate through each dataset
-        let keys = Object.keys(datasets);
-        for (let i = 0; i < keys.length - 1; i++) {
-            let key = keys[i];
-            let dataset = datasets[key];
-            let columns = Object.keys(dataset);
+    if (selectedAreas.length == 0) {
+        // clear the list of areas at the bottom of the map
+        areaList.innerHTML = "<span class='area'>No area selected</span>";
+        results.innerHTML = "Select a precinct or use the dropdown to see results.";
+        legendDetailsLocal.innerHTML = "0";
+        return;
+    }
+
+    // change list based on selected areas
+    let areaListHTML = ""
+    for (let i = 0; i < selectedAreas.length; i++) {
+        areaListHTML += "<span class='area'>" + selectedAreas[i] + "</span>"
+    }
+    areaList.innerHTML = areaListHTML + "<button id='clear-button'>Clear selection</button>"
+
+    // calculate and display local voters
+    local_voters = selectedAreas.reduce((acc, area) => acc + datasets['turnout']['votes_cast'][area], 0);
+    legendDetailsLocal.innerHTML = numberWithCommas(local_voters);
         
-            // sum each column - citywide
-            let columnCitySums = columns.map(column => Object.values(dataset[column]).reduce((acc, value) => acc + value, 0));
+    // create clear button
+    var clearButton = document.getElementById("clear-button")
+    clearButton.addEventListener("click", clear);
 
-            // sum each column - local
-            let localColumnSums = columns.map(column =>
-                selectedAreas.reduce((localAcc, area) => localAcc + dataset[column][area], 0)
-            );
-
-            // create local rates by dividing each column by the local sum
-            const totalLocalSum = localColumnSums[localColumnSums.length - 1];
-            localColumnSums = localColumnSums.slice(0, -1); // trim the last value, which is the total
-            columns = columns.slice(0, -1); // trim the last value, which is the total
-            const localRates = columns.map((_, j) => (localColumnSums[j] / totalLocalSum) * 100);
-            
-            // create citywide rates by dividing each column by the citywide sum
-            const totalCitySum = columnCitySums[columnCitySums.length - 1];
-            columnCitySums = columnCitySums.slice(0, -1); // trim the last value, which is the total
-            const cityRates = columns.map((_, j) => (columnCitySums[j] / totalCitySum) * 100);
-
-            // create chart
-            if (localColumnSums[0] > 0) {
-                var HTML = "<h4>" + fullnames[key][0] + "</h4>" + '<div class="chart" id="chart-' + key + '">' + '<p><em>' + fullnames[key][1] + '</em></p>';
-                for (let i = 0; i < columns.length; i++) {
-                    HTML +=
-                    '<div class="glass">' +
-                        `<p class="bar-label" id="label-${removeSpaces(key+columns[i])}"></p>` +
-                        `<div class="progress-citywide" id="progress-citywide-${removeSpaces(key+columns[i])}"></div>` +
-                        `<div class="progress-local" id="progress-local-${removeSpaces(key+columns[i])}"></div>` +
-                        `<div class="mark-text" id="mark-text-${removeSpaces(key+columns[i])}"></div>` +
-                    '</div>';
-                };
-                HTML += '</div><hr>';
-                results.innerHTML += HTML;
+    // iterate through each dataset
+    let keys = Object.keys(datasets);
+    for (let i = 0; i < keys.length - 1; i++) {
+        let key = keys[i];
+        let dataset = datasets[key];
+        let columns = Object.keys(dataset);
     
-                // match values to chart
-                for (let i = 0; i < columns.length; i++) {
-                    document.getElementById("progress-local-" + removeSpaces(key+columns[i])).style.width = String(localRates[i]) + "%";
-                    document.getElementById("mark-text-" + removeSpaces(key+columns[i])).style.left = String(localRates[i]) + "%";
-                    document.getElementById("mark-text-" + removeSpaces(key+columns[i])).innerHTML = "<span class='bar-highlight local-highlight'>" + String(round(localRates[i]),0) + "%</span>";
-            
-                    document.getElementById("progress-citywide-" + removeSpaces(key+columns[i])).style.width = String(cityRates[i]) + "%";
-                    document.getElementById("label-" + removeSpaces(key+columns[i])).innerHTML = "<strong>" + toTitleCase(columns[i]) + "</strong>" + " (<span class='overall-highlight'>" + String(round(cityRates[i]),0) + "%</span>)";
-    
-                    // set height of chart, depending on if there is a fullnames[key][1]
-                    if (fullnames[key][1] == '') {
-                        document.getElementById("chart-" + key).style.height = String((80 * columns.length) - 10) + "px";
-                    } else {
-                        document.getElementById("chart-" + key).style.height = String((80 * columns.length) + 20) + "px";
-                    }
+        // sum each column - citywide
+        let columnCitySums = columns.map(column => Object.values(dataset[column]).reduce((acc, value) => acc + value, 0));
+
+        // sum each column - local
+        let localColumnSums = columns.map(column =>
+            selectedAreas.reduce((localAcc, area) => localAcc + dataset[column][area], 0)
+        );
+
+        // create local rates by dividing each column by the local sum
+        const totalLocalSum = localColumnSums[localColumnSums.length - 1];
+        localColumnSums = localColumnSums.slice(0, -1); // trim the last value, which is the total
+        columns = columns.slice(0, -1); // trim the last value, which is the total
+        const localRates = columns.map((_, j) => (localColumnSums[j] / totalLocalSum) * 100);
+        
+        // create citywide rates by dividing each column by the citywide sum
+        const totalCitySum = columnCitySums[columnCitySums.length - 1];
+        columnCitySums = columnCitySums.slice(0, -1); // trim the last value, which is the total
+        const cityRates = columns.map((_, j) => (columnCitySums[j] / totalCitySum) * 100);
+
+        // create chart
+        if (localColumnSums[0] > 0) {
+            var HTML = "<h4>" + fullnames[key][0] + "</h4>" + '<div class="chart" id="chart-' + key + '">' + '<p><em>' + fullnames[key][1] + '</em></p>';
+            for (let i = 0; i < columns.length; i++) {
+                HTML +=
+                '<div class="glass">' +
+                    `<p class="bar-label" id="label-${removeSpaces(key+columns[i])}"></p>` +
+                    `<div class="progress-citywide" id="progress-citywide-${removeSpaces(key+columns[i])}"></div>` +
+                    `<div class="progress-local" id="progress-local-${removeSpaces(key+columns[i])}"></div>` +
+                    `<div class="mark-text" id="mark-text-${removeSpaces(key+columns[i])}"></div>` +
+                '</div>';
+            };
+            HTML += '</div><hr>';
+            results.innerHTML += HTML;
+
+            // match values to chart
+            for (let i = 0; i < columns.length; i++) {
+                document.getElementById("progress-local-" + removeSpaces(key+columns[i])).style.width = String(localRates[i]) + "%";
+                document.getElementById("mark-text-" + removeSpaces(key+columns[i])).style.left = String(localRates[i]) + "%";
+                document.getElementById("mark-text-" + removeSpaces(key+columns[i])).innerHTML = "<span class='bar-highlight local-highlight'>" + String(round(localRates[i]),0) + "%</span>";
+        
+                document.getElementById("progress-citywide-" + removeSpaces(key+columns[i])).style.width = String(cityRates[i]) + "%";
+                document.getElementById("label-" + removeSpaces(key+columns[i])).innerHTML = "<strong>" + toTitleCase(columns[i]) + "</strong>" + " (<span class='overall-highlight'>" + String(round(cityRates[i]),0) + "%</span>)";
+
+                // set height of chart, depending on if there is a fullnames[key][1]
+                if (fullnames[key][1] == '') {
+                    document.getElementById("chart-" + key).style.height = String((80 * columns.length) - 10) + "px";
+                } else {
+                    document.getElementById("chart-" + key).style.height = String((80 * columns.length) + 20) + "px";
                 }
             }
         }
-
-        // change pym height
-        delay(250).then(() => pymChild.sendHeight());
-
-        // get turnout sum in selected areas
-        let local_voters = selectedAreas.reduce((localAcc, area) => localAcc + datasets['turnout']['votes_cast'][area], 0);
-        legendDetails.innerHTML = "<span class='local-highlight legend-label'>" + numberWithCommas(local_voters) + "</span> out of <span class='overall-highlight legend-label'>" + numberWithCommas(total_voters) + "</span>"
     }
-}
-
-// function to check if an area is selected
-function validation(data) {
-
-    // sum each value in data
-    total_voters = Object.values(data['votes_cast']).reduce((acc, value) => acc + value, 0);
 
     // change pym height
     delay(250).then(() => pymChild.sendHeight());
 
-    if (selectedAreas == '') {
-        areaList.innerHTML = "<span class='area'>No area selected</span>"
-        results.innerHTML = "<p>Please select a precinct to see results.</p>"
-        legendDetails.innerHTML = "<span class='local-highlight legend-label'>0</span> out of <span class='overall-highlight legend-label'>" + numberWithCommas(total_voters) + "</span>";
-        return false;
-    }
-    else {
-        return true;
-    }
+    // get turnout sum in selected areas
+    legendDetailsLocal.innerHTML = numberWithCommas(local_voters);
 }
 
 // function to clear everything
 function clear() {
-
     // clear the map
     selectedAreas.forEach(function (area) {
-        changeAreaSelect(source, area, false)
+        changeMapSelection([area], false)
     });
     selectedAreas = [];
 
     // clear the list of areas at the bottom of the map
     areaList.innerHTML = "<span class='area'>No area selected</span>"
-    results.innerHTML = ""
-    legendDetails.innerHTML = "<span class='local-highlight legend-label'>0</span> out of <span class='overall-highlight legend-label'>" + numberWithCommas(total_voters) + "</span>";
+    results.innerHTML = "Select a precinct or use the dropdown to see results."
+    legendDetailsLocal.innerHTML = "0"
     
     // change pym height
     delay(250).then(() => pymChild.sendHeight());
@@ -249,14 +276,6 @@ return mapOutlineDetails;
 ///
 /// SECONDARY FUNCTIONS
 ///
-
-// function to set parcel color on click
-function changeAreaSelect(source, hoveredId, bool) {
-    map.setFeatureState(
-        { source: source, id: hoveredId },
-        { selected: bool }
-    );
-}
 
 // function to remove spaces
 function removeSpaces(inputString) {
@@ -342,6 +361,22 @@ async function fetchData(files) {
         datasets[files[i]] = data;
     }
     return datasets
+}
+
+// function to fetch csv
+async function fetchCSV(file) {
+    var response = await fetch('data/'+file+'.csv?nocache='  + (new Date()).getTime());
+    var lookup = await response.text();
+    // turn text into object
+    lookup = lookup.split('\n').map(row => row.split(','));
+    lookup = lookup.map(row => {
+        let obj = {}
+        for (let i = 0; i < row.length; i++) {
+            obj[lookup[0][i]] = row[i]
+        }
+        return obj
+    });
+    return lookup
 }
 
 // trigger hover effects when entering area
