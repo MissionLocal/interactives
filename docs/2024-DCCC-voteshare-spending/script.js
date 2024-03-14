3e75cc3ee3e8a4c0e6e76c75aa182e98d0552d9f
kellyawaldron <116761579+kellywaldro@users.noreply.github.com>
2024-03-14 16:25:25 -0700

    change votes to voteshare

--- docs/2024-DCCC-voteshare-spending/script.js
+++ docs/2024-DCCC-voteshare-spending/script.js
@@ -1,6 +1,11 @@
 // Load the CSV data
 d3.csv("dccc_data.csv").then(function (data) {
 
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 20, left: 30},
+  width = width - margin.left - margin.right,
+  height = height - margin.top - margin.bottom;
+
 // Set up the SVG canvas
 var margin = { top: 20, right: 30, bottom: 30, left: 75 },
     width = 800 - margin.left - margin.right,
@@ -23,7 +28,7 @@ var xScale = d3.scaleLinear()
     .range([0, width]);
 
 var yScale = d3.scaleLinear()
-    .domain([0, 20000])
+    .domain([0, 55])
     .range([height, 0]);
 
 var colorScale = d3.scaleOrdinal()
@@ -75,7 +80,7 @@ svg.selectAll("circle")
     .attr("cx", function (d) { return xScale(d.amount_raised); })  // Switched to amount_raised for x-coordinate
     .attr("cy", function (d) { return yScale(d.votes); })          // Switched to votes for y-coordinate
     .attr("r", 7)   
-    .attr("stroke", function(d) { return d.won === "True" ? "#000" : "none"; }) // Add stroke if "won" is "True"
+    .attr("stroke", function(d) { return d.won === "TRUE" ? "#000" : "none"; }) // Add stroke if "won" is "True"
     .attr("stroke-width", 1.5) // Set stroke width
     .attr("fill", function (d) { return colorScale(d.slate); })    // Color based on "slate" column
     .attr("fill-opacity", 0.6) // Change fill opacity based on "won" column
@@ -83,7 +88,7 @@ svg.selectAll("circle")
         tooltip.transition()
             .duration(200)
             .style("opacity", .9);
-        var amountPerVote = d.amount_raised / d.votes;
+        var amountPerVote = d.amount_raised / d.count;
         tooltip.html("<b>" + d.contest + "</b><br>" +
             "Cost per vote: <b>$" + amountPerVote.toFixed(2) + "</b>")
             .style("left", (d3.event.pageX) + "px")
@@ -107,7 +112,7 @@ var tooltip = d3.select("body").append("div")
     .attr("id", "adjustments"); // Add id for styling
 
     // Define legend data
-var legendData = [{ label: "Won", color: "#000", won: true }].concat(
+var legendData = [{ label: "Won", color: "#000", won: "TRUE" }].concat(
     data.reduce(function(acc, d) {
         if (!acc.find(function(item) { return item.color === colorScale(d.slate); })) {
             acc.push({ label: d.slate, color: colorScale(d.slate), won: false });
@@ -127,7 +132,7 @@ var legendData = [
 // Append legend
 var legend = svg.append("g")
     .attr("class", "legend")
-    .attr("transform", "translate(" + (width - 200) + "," + (height - 150) + ")");
+    .attr("transform", "translate(" + (width - 200) + "," + (height - 100) + ")");
 
 // Add legend circles
 legend.selectAll(".legend-circle")
