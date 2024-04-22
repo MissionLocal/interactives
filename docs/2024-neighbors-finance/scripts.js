01616c5e0d2188d8bbe41fbdb117940a387bee09
kellyawaldron <116761579+kellywaldro@users.noreply.github.com>
2024-04-22 16:55:11 -0700

    making page responsive

--- docs/2024-neighbors-finance/scripts.js
+++ docs/2024-neighbors-finance/scripts.js
@@ -2,8 +2,8 @@
 /// define constants
 ///
 
-const svg_max_width = 1200;
-const svg_max_height = 800;
+const svg_max_width = window.innerWidth;
+const svg_max_height = 1000;
 const svg_margin = 10;
 const node_radius_min = 0;
 const node_radius_max = 60;
