71c80e705c99b07364b5b348f2d0db8b7002ee1d
kellyawaldron <116761579+kellywaldro@users.noreply.github.com>
2025-07-18 15:58:48 -0700

    Update script.js

--- docs/2024-march-full-dccc-slates-map/script.js
+++ docs/2024-march-full-dccc-slates-map/script.js
@@ -3,7 +3,7 @@
 ////
 
 // define access token
-mapboxgl.accessToken = "pk.eyJ1IjoibWxub3ciLCJhIjoiY2t0d2FsdWRpMmkxbDMxcnJ4eTNsMmFlMiJ9.dUju5BD_HqseLNWGIGvXpg";
+mapboxgl.accessToken = "pk.eyJ1IjoibWxub3ciLCJhIjoiY21kNmw1aTAyMDFkbTJqb3Z2dTN0YzRjMyJ9.4abRTnHdhMI-RE48dHNtYw";
 
 // define basemap
 if (window.innerWidth < 400) {
